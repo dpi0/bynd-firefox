991c8ba6d7a06d680a3f5c6f6d62572234f80abd
divyansh <dpi0.dev@proton.me>
2025-07-07 02:16:44 +0530

    fix(tab-nav): spawn previous stashed tabs when switching tabs

--- addon/background.js
+++ addon/background.js
@@ -1,27 +1,32 @@
-browser.commands.onCommand.addListener((command) => {
+browser.commands.onCommand.addListener(async (command) => {
   switch (command) {
     case "prev-tab":
-      browser.tabs.query({ currentWindow: true }, (tabs) => {
-        if (tabs.length <= 1) return;
-
-        const currentIndex = tabs.findIndex((tab) => tab.active);
-        const prevIndex =
-          currentIndex === 0 ? tabs.length - 1 : currentIndex - 1;
-
-        browser.tabs.update(tabs[prevIndex].id, { active: true });
+      const tabs = await browser.tabs.query({
+        currentWindow: true,
+        hidden: false,
       });
+
+      if (tabs.length <= 1) return;
+
+      const currentIndex = tabs.findIndex((tab) => tab.active);
+      const prevIndex = currentIndex === 0 ? tabs.length - 1 : currentIndex - 1;
+
+      browser.tabs.update(tabs[prevIndex].id, { active: true });
       break;
 
     case "next-tab":
-      browser.tabs.query({ currentWindow: true }, (tabs) => {
-        if (tabs.length <= 1) return;
-
-        const currentIndex = tabs.findIndex((tab) => tab.active);
-        const nextIndex =
-          currentIndex === tabs.length - 1 ? 0 : currentIndex + 1;
-
-        browser.tabs.update(tabs[nextIndex].id, { active: true });
+      const tabs2 = await browser.tabs.query({
+        currentWindow: true,
+        hidden: false,
       });
+
+      if (tabs2.length <= 1) return;
+
+      const currentIndex2 = tabs2.findIndex((tab) => tab.active);
+      const nextIndex =
+        currentIndex2 === tabs2.length - 1 ? 0 : currentIndex2 + 1;
+
+      browser.tabs.update(tabs2[nextIndex].id, { active: true });
       break;
 
     case "new-tab":
@@ -29,11 +34,12 @@ browser.commands.onCommand.addListener((command) => {
       break;
 
     case "close-tab":
-      browser.tabs.query({ active: true, currentWindow: true }, (tabs) => {
-        if (tabs.length > 0) {
-          browser.tabs.remove(tabs[0].id);
-        }
+      const [tab] = await browser.tabs.query({
+        active: true,
+        currentWindow: true,
       });
+
+      if (tab) browser.tabs.remove(tab.id);
       break;
   }
 });
